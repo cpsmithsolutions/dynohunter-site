8e950e0ea0c3fc51c4910996fcc3b46400a17c5e
clarkpsmith <clark.smith79@gmail.com>
2023-06-27 17:34:31 -0600

    update photo alt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <NextSeo
         title="DYNOHUNTER | Tribal House & Techno"
         description="DYNOHUNTER is a tribal house and techno project created by DJ, Producer, Saxophonist and Impressions Records Label Boss Clark Smith. Smith layers tribal beats, ethereal textures, and searing synths over deep groovy basslines, using his Saxophone as the protagonist in the eclectic and exotic stories he shares."
+
+
         // canonical=""
         openGraph={{
           type: 'website',
@@ -20,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
               url: `https://images.ctfassets.net/mrg9gspwlszz/5MfxKhizQfJNoeGRmDB0d2/6084441de665425c046dfd292c5b97ab/DSC07496_50-low-res.jpg`,
               width: 250,
               height: 250,
-              alt: 'DYNOHUNTER Logo',
+              alt: 'DYNOHUNTER Photo',
             },
           ],
           site_name: 'DYNOHUNTER | Tribal House & Techno',
