4b558a5b062f7b58e6c55b5c17d07657f67486d8
clarkpsmith <clark.smith79@gmail.com>
2023-07-11 16:49:50 -0600

    remove cursor pointer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,7 @@ export default function Home({tourDates, showPhotos, previewImage}: {tourDates: 
       </div>
       <div className="flex p-10 pt-6 justify-around items-end">
        <div className="mb-2">
-       <div className="cursor-pointer">
+       <div className="">
         <Image priority={true} width={350} height={350} alt="dynohunter logo"  src="/images/dynohunter-logo.png"/>
         </div>  
        </div>
