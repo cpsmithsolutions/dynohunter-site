bb37ed96dd6aab9aa0ccded564f37062a8230eb0
clarkpsmith <clark.smith79@gmail.com>
2024-05-10 18:31:08 -0600

    back out

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
       <meta name="facebook-domain-verification" content="kror03mt5ttm7jjjujnm3aoz38kvr2" />
-      <script src="./utilities/facebook-tracking.js" async></script>
+      <script src="../utilities/facebook-tracking.js" async></script>
         </Head>
       <body>
         <Main />
