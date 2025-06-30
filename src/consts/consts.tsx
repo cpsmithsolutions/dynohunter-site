635d1a6531df8715a2317da522809eecb54189a5
clarkpsmith <clark.smith79@gmail.com>
2025-06-29 19:46:47 -0600

    remove commas

--- src/consts/consts.tsx
+++ src/consts/consts.tsx
@@ -1,6 +1,6 @@
-export const BIO =  <p>DYNOHUNTER is the tribal techno project of DJ, producer, and saxophonist Clark Smith, blending ancient rhythms with futuristic soundscapes. Layering syncopated percussion, hypnotic grooves, and searing synths over deep, rolling basslines, DYNOHUNTER conjures sonic rituals that blur the line between primal instinct and dance floor surrender.
+export const BIO =  <p>DYNOHUNTER is the tribal techno project of DJ, producer, and saxophonist Clark Smith, blending ancient rhythms with futuristic soundscapes. Layering syncopated percussion, hypnotic grooves, and searing synths over deep rolling basslines, DYNOHUNTER conjures sonic rituals that blur the line between primal instinct and dance floor surrender.
 
-Fueled by raw intensity, Smith’s music pulses with trance inducing energy—both sacred ceremony and sweat-drenched rave. His tenor saxophone shimmers like a flame in the night, a bold, human voice cutting through the lush, shadowed depths of his tribal soundscapes.
+Fueled by raw intensity, Smith’s music pulses with trance inducing energy—both sacred ceremony and sweat-drenched rave. His tenor saxophone shimmers like a flame in the night, a bold human voice cutting through the lush shadowed depths of his tribal soundscapes.
 
 At its core, DYNOHUNTER is a fierce celebration of the primal and the untamed, an invitation to lose yourself in the wild, the mystical, and the timeless rhythm within.</p>
 
