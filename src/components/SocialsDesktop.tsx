cbe90798677004fd72dacb3e9e472e182c732cb0
clarkpsmith <clark.smith79@gmail.com>
2025-09-12 22:53:11 -0600

    padding issues

--- src/components/SocialsDesktop.tsx
+++ src/components/SocialsDesktop.tsx
@@ -39,7 +39,7 @@ const SocialsDesktop = () => {
 </div>
 <a target="_blank" href="https://www.beatport.com/artist/dynohunter/575899">
 <div className="">
-<div className="cursor-pointer">
+<div className="cursor-pointer pb-1">
 <Image alt="beatport logo" width={110} height={50} src="/images/beatport-white.png"/>
 </div>
 </div>
